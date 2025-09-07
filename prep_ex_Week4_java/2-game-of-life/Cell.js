1af0b9826cf690c2cbef636034afc91021b64a1a
Majoodeh <Majdhussein144@gmail.com>
2025-09-07 09:58:34 +0200

    Add files via upload

---
+++ prep_ex_Week4_java/2-game-of-life/Cell.js
@@ -0,0 +1,54 @@
+export default class Cell {
+  static size;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.alive = Math.random() > 0.5;
+    this.nextAlive = false;
+    this.lifeTime = this.alive ? 1 : 0;
+  }
+
+  draw(context) {
+    context.fillStyle = "#303030";
+    context.fillRect(
+      this.x * Cell.size,
+      this.y * Cell.size,
+      Cell.size,
+      Cell.size
+    );
+
+    if (this.alive) {
+      let opacity;
+      if (this.lifeTime === 1) opacity = 0.25;
+      else if (this.lifeTime === 2) opacity = 0.5;
+      else if (this.lifeTime === 3) opacity = 0.75;
+      else opacity = 1;
+
+      context.fillStyle = `rgba(24, 215, 236, ${opacity})`;
+      context.fillRect(
+        this.x * Cell.size + 1,
+        this.y * Cell.size + 1,
+        Cell.size - 2,
+        Cell.size - 2
+      );
+    }
+  }
+
+  liveAndLetDie(aliveNeighbors) {
+    if (aliveNeighbors === 2) {
+      this.nextAlive = this.alive;
+      if (this.alive) this.lifeTime += 1;
+    } else if (aliveNeighbors === 3) {
+      this.nextAlive = true;
+      this.lifeTime = this.alive ? this.lifeTime + 1 : 1;
+    } else {
+      this.nextAlive = false;
+      this.lifeTime = 0;
+    }
+  }
+
+  update() {
+    this.alive = this.nextAlive;
+  }
+}
